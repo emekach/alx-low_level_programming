71f676a200ab205a8a656fdc9c5d688bf30e1fa1
emekach <chukwuemeka.chukwuma11@gmail.com>
2022-10-13 09:53:54 -0700

    Using puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-    char string[]="\"Programming is like building a multilingual puzzle";
-    puts(string);
-    return (0);
+	char string[] = "\"Programming is like building a multilingual puzzle";
 
+	puts(string);
+	return (0);
 }
