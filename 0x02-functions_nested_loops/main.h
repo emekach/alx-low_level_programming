2ba622529c8f9480e68f1b00fa4cc40635abb252
emekach <chukwuemeka.chukwuma11@gmail.com>
2022-10-18 09:54:04 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+
+#ifndef _MAIN_H_INCLUDED
+#define _MATH_H_INCLUDED
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int c);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+void times_table(void);
+void print_times_table(int n);
+#endif
