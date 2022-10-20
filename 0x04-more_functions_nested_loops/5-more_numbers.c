4c7d1882e930aa8a3e581d87d71f20692bd504d4
emekach <chukwuemeka.chukwuma11@gmail.com>
2022-10-20 14:24:54 -0700

    print ,ore numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* more_numbers - print 0-14 in 10 lines
+*
+* Return: 0-14 ten times if succefully
+*/
+
+void more_numbers(void)
+{
+	int i1;
+	char i2, c;
+
+	for (i1 = 0; i1 < 10; i1++)
+	{
+		for (i2 = 0; i2 <= 14; i2++)
+		{
+			c = i2;
+			if (i2 > 9)
+			{
+				_putchar('1');
+				c = i2 % 10;
+			}
+
+			_putchar('0' + c);
+		}
+
+		_putchar('\n');
+	}
+}
