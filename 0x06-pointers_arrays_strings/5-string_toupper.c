8f7780a6ffdd647aff5d132f253c7cd13d2ce846
emekach <chukwuemeka.chukwuma11@gmail.com>
2022-10-26 14:02:25 -0700

    converts to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* string_toupper - convert to uppercase
+* @x: string parameter
+* Return: a string
+*/
+
+char *string_toupper(char *x)
+{
+	int i;
+
+	for (i = 0; x[i] != '\0'; i++)
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+		{
+			x[i] = x[i] - 32;
+		}
+	}
+	return (x);
+}
