e0999379c072e368489fb66286b139ef830c1a59
emekach <chukwuemeka.chukwuma11@gmail.com>
2022-11-04 16:43:04 -0700

    static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef MAIN_H_
+#define MAIN_H_
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -11,10 +10,10 @@ int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest,  char *src );
-char *_strncat(char *dest,  char *src, int n);
-char *_strncpy(char *dest,  char *src, int n);
-int  _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
